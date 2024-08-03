30e620929bd4cb763b20fe2d2c70e33f6ee4f481
omatviiv <oleh.matviiv@gmail.com>
2024-08-03 11:57:40 +0200

    add typechecking, improve svgo approach, upgrade packages

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import trident from 'src/assets/trident.svg?url';
-import Trident from 'src/assets/trident.svg';
+import trident from 'src/asset/trident.svg?url';
+import Trident from 'src/asset/trident.svg';
 import {Container, Header, Image} from './style';
 
 const Application: React.FC = () => <Container>
